6dec1c306b878b9dd75175fe04b2d0c7b8181684
alimdfaiz <alimdfaiz6@gmail.com>
2024-01-20 20:03:39 +0530

    signup form

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./style.css";
+import App from "./App";
+import ReactDOM from "react-dom";
+
+
+
+
+
+
+
+
+
+
+
+ReactDOM.render(<App />, document.getElementById("root"));
